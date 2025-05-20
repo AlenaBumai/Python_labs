f7ebe7564fff42ce4928c6d657adfe6183fccb74
AlenaBumai <BUMAI.ALENA@GMAIL.COM>
2025-05-20 15:17:27 +0300

    lab_10 новости с авторизацией
    новости с авторизацией
    Topic -темы новостей
    NewsText - конкретные новости, привязанные к конкретной теме

--- FinalProject/news/urls.py
+++ FinalProject/news/urls.py
@@ -8,4 +8,12 @@ urlpatterns = [
 
     path('contacts/', views.contacts, name='contacts'),
     path('topic/<int:topic_id>/', views.newstext, name='newstext'),
+
+
+# авторизация и регистрация пользователей
+    re_path('^register/$', views.RegisterFormView.as_view()),
+    re_path('^login/$', views.LoginFormView.as_view()),
+    re_path('^logout/$', views.LogoutView.as_view()),
+    re_path('^password-change/', views.PasswordChangeView.as_view())
+
 ]
--- FinalProject/news/views.py
+++ FinalProject/news/views.py
@@ -2,7 +2,19 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseNotFound
 from .models import Topic, NewsText
 
+from django.views.generic.edit import FormView
+from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
 
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
+
+from django.http import HttpResponseRedirect
+from django.views.generic.base import View
+from django.contrib.auth import logout
+
+
+
+app_url="/news/"
 def index(request):
     topics = Topic.objects.all()
     return render(request, "index.html", {'topics': topics})
@@ -13,3 +25,52 @@ def newstext(request, topic_id):
     return render(request, "newstext.html", {'topic': topic, 'news_texts': news_texts})
 def contacts(request):
     return render(request, "contacts.html")
+
+# авторизация и регистрация пользователей
+class RegisterFormView(FormView):
+
+    form_class = UserCreationForm
+
+    success_url = app_url + "login/"
+
+    template_name = "reg/register.html"
+    def form_valid(self, form):
+        form.save()
+        return super(RegisterFormView, self).form_valid(form)
+
+# вход - логирование
+
+class LoginFormView(FormView):
+    form_class = AuthenticationForm
+    template_name = "reg/login.html"
+
+    success_url = app_url
+    def form_valid(self, form):
+        self.user = form.get_user()
+        login(self.request, self.user)
+        return super(LoginFormView, self).form_valid(form)
+
+# для выхода - миниатюрное представление без шаблона -
+# после выхода перенаправим на главную
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return HttpResponseRedirect(app_url)
+
+# изменение пароля
+class PasswordChangeView(FormView):
+
+    form_class = PasswordChangeForm
+    template_name = 'reg/password_change_form.html'
+
+    success_url = app_url + 'login/'
+    def get_form_kwargs(self):
+        kwargs = super(PasswordChangeView, self).get_form_kwargs()
+        kwargs['user'] = self.request.user
+        if self.request.method == 'POST':
+            kwargs['data'] = self.request.POST
+        return kwargs
+    def form_valid(self, form):
+        form.save()
+        return super(PasswordChangeView, self).form_valid(form)
+
